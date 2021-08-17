b368481d10de54d1c2e7e9c6d27df99866e4c55c
Muhammad Rifqy <hadimrifqyfakhrul@gmail.com>
2021-08-17 15:10:52 +0700

    up

--- helpers/learnReplies.js
+++ helpers/learnReplies.js
@@ -1,186 +1,176 @@
-const fs = require('fs');
-const { Stemmer,Tokenizer } = require('sastrawijs');
+const fs = require("fs");
+const { Stemmer, Tokenizer } = require("sastrawijs");
 
-const alternative = [
-    "Same",
-    "Go on...",
-    "Bro...",
-    "Try again",
-    "I don't understand :/"
-]
+const alternative = ["Same", "Go on...", "Bro...", "Try again", "I don't understand :/"];
 
-const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"];
 
-
-function ganti(params,data) {
-    return params.replace(
-        /%(\w*)%/g, // or /{(\w*)}/g for "{this} instead of %this%"
-        function( m, key ){
-            return data.hasOwnProperty( key ) ? data[ key ] : "";
-        }
-    )
+function ganti(params, data) {
+  return params.replace(
+    /%(\w*)%/g, // or /{(\w*)}/g for "{this} instead of %this%"
+    function (m, key) {
+      return data.hasOwnProperty(key) ? data[key] : "";
+    }
+  );
 }
 
-function tokenisasi(sentence) {
+function tokenize(sentence) {
   var stemmed = [];
-    //   const custom = ['ga','ga'];
+  //   const custom = ['ga','ga'];
   var stemmer = new Stemmer();
   var tokenizer = new Tokenizer();
   words = tokenizer.tokenize(sentence);
   for (word of words) {
     stemmed.push(stemmer.stem(word));
   }
-  return stemmed.join(' ');
+  return stemmed.join(" ");
 }
 
+function fuzzy_match(text, search) {
+  search = tokenize(search.replace(/\ /g, " ").toLowerCase());
+  // console.log(search);
+  var tokens = [];
+  var search_position = 0;
+  charValue = [];
 
-function fuzzy_match(text, search)
-{
-    search = tokenisasi(search.replace(/\ /g, ' ').toLowerCase());
-    // console.log(search);
-    var tokens = [];
-    var search_position = 0;
-    charValue = [];
+  // text = ganti(text,{simply_minghwa:'contoh sederhana'});
 
-    // text = ganti(text,{simply_minghwa:'contoh sederhana'});
+  textGab = "";
 
-    textGab = '';
+  if ((check = text.indexOf(search))) {
+    textGab = text.substring(0, check);
+    text = text.substring(check);
+  }
 
+  for (var n = 0; n < text.length; n++) {
+    var text_char = text[n];
+    if (search_position < search.length && text_char.toLowerCase() == search[search_position]) {
+      // text_char = '<b>' + text_char + '</b>';
+      charValue.push(text_char);
+      if (charValue.includes(n - 1)) charValue.push(`${n - 1}*1`);
 
-    if (check = text.indexOf(search)) {
-        textGab = text.substring(0,check);
-        text = text.substring(check);
+      search_position += 1;
     }
-    
-    for (var n=0; n<text.length; n++)
-    {
-        var text_char = text[n];
-        if(search_position < search.length &&
-        text_char.toLowerCase() == search[search_position])
-        {
-            // text_char = '<b>' + text_char + '</b>';
-            charValue.push(text_char);
-            if (charValue.includes(n-1)) charValue.push(`${n-1}*1`)
+    tokens.push(text_char);
+  }
 
-            search_position += 1;
-        }
-        tokens.push(text_char);
-    }
+  // console.log(search_position,text.length,search.length);
 
-    // console.log(search_position,text.length,search.length);
+  if (search_position > search.length) return { text: "", value: 0 };
 
-    if (search_position > search.length) return {text:'',value:0};
+  // return tokens.join('');
+  joinText = textGab + tokens.join("").replace(/<[^>]*>?/gm, "");
 
-    // return tokens.join('');
-    joinText = textGab+tokens.join('').replace(/<[^>]*>?/gm, '');
+  let total = charValue.length;
 
-    let total = charValue.length;
-    
-    // if (textGab.length > 0) {
-    //     total += textGab.length
-    // }
+  // if (textGab.length > 0) {
+  //     total += textGab.length
+  // }
 
-    let value = (total/joinText.length)*(100).toFixed(2);
+  let value = (total / joinText.length) * (100).toFixed(2);
 
-    // console.log(charValue);
-    return {
-        text:joinText,
-        value:total,
-        total,
-        jt:joinText.length
-    };
+  // console.log(charValue);
+  return {
+    text: joinText,
+    value: total,
+    total,
+    jt: joinText.length,
+  };
 }
 
+var learn = (data) => {
+  const input = data.text;
+  const CONVERSATION_FILE = `./phrases-data/user-${data.user}.json`;
+  let conversations = false;
 
-var learn = (data) =>{
-    const input = data.text;
-    const CONVERSATION_FILE = `./phrases-data/user-${data.user}.json`;
-    let conversations = false;
+  fs.access(CONVERSATION_FILE, (err) => {
+    if (err) {
+      let newFile = fs.readFileSync("./conversations.json");
+      fs.writeFileSync(CONVERSATION_FILE, newFile);
+    }
+  });
 
-    fs.access(CONVERSATION_FILE, (err) => {
-        if (err) {
-            let newFile = fs.readFileSync("./conversations.json");
-            fs.writeFileSync(CONVERSATION_FILE,newFile);
-        }
-    });
-    
-    conversations = JSON.parse(fs.readFileSync(CONVERSATION_FILE))
-    result = false;
-    let results = [];
+  conversations = JSON.parse(fs.readFileSync(CONVERSATION_FILE));
+  result = false;
+  let results = [];
 
-    if (conversations) {
-        //kumpulkan semua pertanyaan
-        const pertanyaans = [].concat.apply(
-            [],
-            //kumpulkan semua pertanyaan ke dalam satu array
-            Object.keys(conversations).map((key, index)=> conversations[key].pertanyaan)
-        );
-    
-        /* 
+  if (conversations) {
+    //kumpulkan semua pertanyaan
+    const pertanyaans = [].concat.apply(
+      [],
+      //kumpulkan semua pertanyaan ke dalam satu array
+      Object.keys(conversations).map((key, index) => conversations[key].pertanyaan)
+    );
+
+    /* 
             proses semua pertanyaan menggunakan fuzzy_match method.
             hasil dari proses fuzzy matching setiap index akan mendapatkan value yang menjadi ukuran akurasi setiap index
         */
-        //    console.log(pertanyaans);
-        pertanyaans.forEach((el,i) => {
-            hasil = fuzzy_match(el,input);
-            results.push(hasil)
-        });
-    
-        //cari value tertinggi
-        const maxValue = Math.max.apply(Math, results.map(function(v) { return v.value; }));
+    //    console.log(pertanyaans);
+    pertanyaans.forEach((el, i) => {
+      hasil = fuzzy_match(el, input);
+      results.push(hasil);
+    });
 
-        console.log({maxValue,results});
+    //cari value tertinggi
+    const maxValue = Math.max.apply(
+      Math,
+      results.map(function (v) {
+        return v.value;
+      })
+    );
 
-        if (maxValue > 0) {
-    
-            let selecteD = results.filter((result) => result.value == maxValue); //ambil index pertanyaan dengan value tertinggi
-            
-            if (selecteD.length > 0) {
-                category = Object.keys(conversations).filter((q,i) => conversations[q].pertanyaan.includes(selecteD[0].text)) //ambil satu pertanyaan terpilih, kemudian cari kategori dari conversations yang mana pertanyaannya mencakup hasil pertanyaan terpilih
-    
-                replies = conversations[category[0]].jawaban; //setelah kategori ditemukan, ambil semua jawabannya
-                result = replies[Math.floor(Math.random() * replies.length)];//pilih jawaban secara random
-            }
-        }
-        
+    console.log({ maxValue, results });
+
+    if (maxValue > 0) {
+      let selecteD = results.filter((result) => result.value == maxValue); //ambil index pertanyaan dengan value tertinggi
+
+      if (selecteD.length > 0) {
+        category = Object.keys(conversations).filter((q, i) => conversations[q].pertanyaan.includes(selecteD[0].text)); //ambil satu pertanyaan terpilih, kemudian cari kategori dari conversations yang mana pertanyaannya mencakup hasil pertanyaan terpilih
+
+        replies = conversations[category[0]].jawaban; //setelah kategori ditemukan, ambil semua jawabannya
+        result = replies[Math.floor(Math.random() * replies.length)]; //pilih jawaban secara random
+      }
     }
+  }
 
-
-    return result;
-}
+  return result;
+};
 
 function output(input) {
-    let answer;
+  let answer;
 
-    // Regex remove non word/space chars
-    // Trim trailing whitespce
-    // Remove digits - not sure if this is best
-    // But solves problem of entering something like 'hi1'
+  // Regex remove non word/space chars
+  // Trim trailing whitespce
+  // Remove digits - not sure if this is best
+  // But solves problem of entering something like 'hi1'
 
-    let text = input.text.toLowerCase().replace(/[^\w\s]/gi, "")
-        .replace(/[\d]/gi, "")
-        .trim()
-        .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
-        .replace(/i feel /g, "")
-        .replace(/whats/g, "what is")
-        .replace(/please /g, "")
-        .replace(/ please/g, "")
-        .replace(/r u/g, "are you");
+  let text = input.text
+    .toLowerCase()
+    .replace(/[^\w\s]/gi, "")
+    .replace(/[\d]/gi, "")
+    .trim()
+    .replace(/ a /g, " ") // 'tell me a story' -> 'tell me story'
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "")
+    .replace(/r u/g, "are you");
 
-    if (learn({text,user:input.user})) {
-        // Search for exact match in `prompts`
-        answer = learn({text,user:input.user});
-    } else if (text.match(/thank/gi) || text.match(/thx/gi)) {
-        answer = "You're welcome!"
-    } else if (text.match(/(corona|covid|virus)/gi)) {
-        // If no match, check if message contains `coronavirus`
-        answer = coronavirus[Math.floor(Math.random() * coronavirus.length)];
-    } else {
-        // If all else fails: random alternative
-        answer = alternative[Math.floor(Math.random() * alternative.length)];
-    }
+  if (learn({ text, user: input.user })) {
+    // Search for exact match in `prompts`
+    answer = learn({ text, user: input.user });
+  } else if (text.match(/thank/gi) || text.match(/thx/gi)) {
+    answer = "You're welcome!";
+  } else if (text.match(/(corona|covid|virus)/gi)) {
+    // If no match, check if message contains `coronavirus`
+    answer = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+  } else {
+    // If all else fails: random alternative
+    answer = alternative[Math.floor(Math.random() * alternative.length)];
+  }
 
-    return answer;
+  return answer;
 }
 
-module.exports = {output};
+module.exports = { output };
--- init/wa.js
+++ init/wa.js
@@ -1,71 +1,70 @@
-const { Client,Location } = require('whatsapp-web.js');
-const fs = require('fs');
-const Session = require('../models/Sessions');
-const qrcode = require('qrcode');
-const getSession = async id => await Session.findById(id).exec();
+const { Client, Location } = require("whatsapp-web.js");
+const fs = require("fs");
+const Session = require("../models/Sessions");
+const qrcode = require("qrcode");
+const getSession = async (id) => await Session.findById(id).exec();
 const getAllSessions = async () => await Session.find();
 const client = "kempung";
 const { output } = require("../helpers/learnReplies");
 let whatsapp = {};
-const strater = io =>{
-  
-  const createClient = async (data) =>{
-    await Session.create(data, async (err,success)=>{
+const strater = (io) => {
+  const createClient = async (data) => {
+    await Session.create(data, async (err, success) => {
       result = {
-        success:false,
-        user_id:data.user_id,
-        description:data.description
+        success: false,
+        user_id: data.user_id,
+        description: data.description,
       };
-      
-      if (err){
+
+      if (err) {
         result.message = err;
-      }else{
+      } else {
         await createSession(success._id, result.description);
-        result.success=true
+        result.success = true;
         result.message = success;
       }
-      
-      io.emit('create-client',result);
-    })
+
+      io.emit("create-client", result);
+    });
   };
-  
-  const createSession = async function(id) {
-    let sessionCfg=null;
-    const doc = await Session.findOne({_id:id});
-    console.log('Creating session: ' + id);
-    const result = await Session.check({ _id: id});
+
+  const createSession = async function (id) {
+    let sessionCfg = null;
+    const doc = await Session.findOne({ _id: id });
+    console.log("Creating session: " + id);
+    const result = await Session.check({ _id: id });
     if (result) {
       resultClient = await getSession(id);
       if (resultClient.client) {
-          sessionCfg = JSON.parse(resultClient.client);
+        sessionCfg = JSON.parse(resultClient.client);
       }
     }
-    console.log(sessionCfg,'session cfg');
+    console.log(sessionCfg, "session cfg");
     const client = new Client({
       restartOnAuthFail: true,
       puppeteer: {
         headless: true,
         args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-accelerated-2d-canvas',
-          '--no-first-run',
-          '--no-zygote',
-          '--single-process', // <- this one doesn't works in Windows
-          '--disable-gpu'
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+          "--disable-accelerated-2d-canvas",
+          "--no-first-run",
+          "--no-zygote",
+          "--single-process", // <- this one doesn't works in Windows
+          "--disable-gpu",
         ],
       },
-      session: sessionCfg
+      session: sessionCfg,
     });
-  
-    client.on('qr', async (qr) => {
-      console.log('masih qr');
-      const cekClient = await Session.check({ _id: id});
+
+    client.on("qr", async (qr) => {
+      console.log("QR ", qr);
+      const cekClient = await Session.check({ _id: id });
       if (cekClient) {
         qrcode.toDataURL(qr, (err, url) => {
-          io.emit('qr', { id: id, src: url });
-          io.emit('message', { id: id, text: 'QR Code received, scan please!' });
+          io.emit("qr", { id: id, src: url });
+          io.emit("message", { id: id, text: "QR Code received, scan please!" });
         });
       }
       // else{
@@ -73,75 +72,73 @@ const strater = io =>{
       // }
     });
 
-    client.on('ready', () => {
-      
-      io.emit('ready', { id: id });
-      io.emit('message', { id: id, text: 'Whatsapp is ready!' });
+    client.on("ready", () => {
+      io.emit("ready", { id: id });
+      io.emit("message", { id: id, text: "Whatsapp is ready!" });
 
       if (result) {
         doc.ready = true;
         doc.save();
       }
     });
-  
-    client.on('authenticated', (session) => {
-      
-      io.emit('authenticated', id);
-      io.emit('message', { id: id, text: 'Whatsapp is authenticated!' });
+
+    client.on("authenticated", (session) => {
+      io.emit("authenticated", id);
+      io.emit("message", { id: id, text: "Whatsapp is authenticated!" });
       sessionCfg = session;
       if (result) {
         doc.client = JSON.stringify(session);
         doc.save();
         whatsapp[id] = client;
       }
-    })
-    
-    client.on('message', async msg => {
+    });
+
+    client.on("message", async (msg) => {
       const chat = await msg.getChat();
 
-      if (msg.body == '!ping') {
-        msg.reply('pong');
-      }else if (msg.body == '!groups') {
-        client.getChats().then(chats => {
-          const groups = chats.filter(chat => chat.isGroup);
-    
+      if (msg.body == "!ping") {
+        msg.reply("pong");
+      } else if (msg.body == "!groups") {
+        client.getChats().then((chats) => {
+          const groups = chats.filter((chat) => chat.isGroup);
+
           if (groups.length == 0) {
-            msg.reply('You have no group yet.');
+            msg.reply("You have no group yet.");
           } else {
-            let replyMsg = '*YOUR GROUPS*\n\n';
+            let replyMsg = "*YOUR GROUPS*\n\n";
             groups.forEach((group, i) => {
               replyMsg += `ID: ${group.id._serialized}\nName: ${group.name}\n\n`;
             });
-            replyMsg += '_You can use the group id to send a message to the group._'
+            replyMsg += "_You can use the group id to send a message to the group._";
             msg.reply(replyMsg);
           }
         });
-      // } else if (msg.body === '!location') {
-      //   msg.reply(new Location(-7.322956673591686, 112.76473464992037, 'Revo Apps\nSurabaya'));
-      // } else if (msg.location) {
-      //     msg.reply(msg.location);
-      } else if (msg.body.startsWith('!status ')) {
-          const newStatus = msg.body.split(' ')[1];
-          await client.setStatus(newStatus);
-          msg.reply(`Status was updated to *${newStatus}*`);
-      } else if (msg.body === '!mention') {
+        // } else if (msg.body === '!location') {
+        //   msg.reply(new Location(-7.322956673591686, 112.76473464992037, 'Revo Apps\nSurabaya'));
+        // } else if (msg.location) {
+        //     msg.reply(msg.location);
+      } else if (msg.body.startsWith("!status ")) {
+        const newStatus = msg.body.split(" ")[1];
+        await client.setStatus(newStatus);
+        msg.reply(`Status was updated to *${newStatus}*`);
+      } else if (msg.body === "!mention") {
         const contact = await msg.getContact();
         chat.sendMessage(`Hi @${contact.number}!`, {
-            mentions: [contact]
+          mentions: [contact],
         });
 
-      // }else if (msg.body === '!evilmorn') {
-      //   const chatGroup = await client.getChatById('6281232072122-1617437698@g.us');
-      //   chatGroup.addParticipants([msg.from]);
-      //   chat.sendMessage(`Hei, you added to group  evilmorning`);
-      }else{
-        const reply = await output({text:msg.body,user:doc.user_id});
-        chat.sendMessage(''+reply);
+        // }else if (msg.body === '!evilmorn') {
+        //   const chatGroup = await client.getChatById('6281232072122-1617437698@g.us');
+        //   chatGroup.addParticipants([msg.from]);
+        //   chat.sendMessage(`Hei, you added to group  evilmorning`);
+      } else {
+        const reply = await output({ text: msg.body, user: doc.user_id });
+        chat.sendMessage("" + reply);
       }
     });
 
-    client.on ('change_state', async (change_state) => {
-      console.log ('WhatsApp: Device', change_state);
+    client.on("change_state", async (change_state) => {
+      console.log("WhatsApp: Device", change_state);
       if (change_state == "PAIRING") {
         doc.client = null;
         doc.ready = false;
@@ -150,16 +147,16 @@ const strater = io =>{
         client.destroy();
         client.initialize();
       }
-      io.emit('message', { id: id, text: 'WhatsApp: Device '+change_state });
+      io.emit("message", { id: id, text: "WhatsApp: Device " + change_state });
     });
 
-    client.on('auth_failure', function(session) {
-      io.emit('message', { id: id, text: 'Auth failure, restarting...' });
+    client.on("auth_failure", function (session) {
+      io.emit("message", { id: id, text: "Auth failure, restarting..." });
     });
-  
-    client.on('disconnected', async (reason) => {
-      io.emit('message', { id: id, text: 'Whatsapp is disconnected!',reason });
-      if (await Session.check({ _id: id})) {
+
+    client.on("disconnected", async (reason) => {
+      io.emit("message", { id: id, text: "Whatsapp is disconnected!", reason });
+      if (await Session.check({ _id: id })) {
         doc.client = null;
         doc.ready = false;
         doc.save();
@@ -167,66 +164,63 @@ const strater = io =>{
         client.destroy();
         client.initialize();
       }
-      io.emit('remove-session', id);
+      io.emit("remove-session", id);
     });
 
-    
     client.initialize();
-    
-  }
+  };
 
-  const init = async function(socket) {
+  const init = async function (socket) {
     const savedSessions = await Session.find({});
 
     if (savedSessions.length > 0) {
       if (socket) {
-        socket.emit('init', savedSessions);
+        socket.emit("init", savedSessions);
       } else {
-        savedSessions.forEach(sess => {
+        savedSessions.forEach((sess) => {
           createSession(sess._id, sess.description);
         });
       }
     }
-  }
+  };
 
   init();
 
   // Socket IO
-  io.on('connection', function(socket) {
+  io.on("connection", function (socket) {
     init(socket);
 
-    socket.on('create-session', async function(data) {
+    socket.on("create-session", async function (data) {
       createClient(data);
     });
 
-    socket.on('remove-session', async function(data) {
+    socket.on("remove-session", async function (data) {
       // const doc = await Session.findOne({_id:data});
-      console.log('removing session: ' + data);
-      const result = await Session.check({ _id: data});
+      console.log("removing session: " + data);
+      const result = await Session.check({ _id: data });
       console.log(result);
       if (result) {
         const client = whatsapp[data];
-        
+
         if (client) {
           client.logout();
           client.destroy();
         }
 
         const res = await Session.deleteOne({ _id: data });
-        console.log('nih setelah di hapus',res);
-        io.emit('create-client');
+        console.log("nih setelah di hapus", res);
+        io.emit("create-client");
         init();
       }
-      
     });
-    socket.on('logout', async function(data) {
+    socket.on("logout", async function (data) {
       // const doc = await Session.findOne({_id:data});
-      console.log('removing session: ' + data);
-      const result = await Session.check({ _id: data});
+      console.log("removing session: " + data);
+      const result = await Session.check({ _id: data });
       console.log(result);
       if (result) {
         const client = whatsapp[data];
-        
+
         if (client) {
           // client.logout();
           client.destroy();
@@ -234,15 +228,11 @@ const strater = io =>{
 
         // const res = await Session.deleteOne({ _id: data });
         // console.log('nih setelah di hapus',res);
-        io.emit('create-client');
+        io.emit("create-client");
         init();
       }
-      
     });
   });
-
-
-      
 };
 
-module.exports = {strater,getAllSessions};
+module.exports = { strater, getAllSessions };
