b63a26606b0e283047dad4059ccac99031fb00c1
Muhammad Rifqy <hadimrifqyfakhrul@gmail.com>
2021-12-03 13:24:37 +0700

    up

---
+++ helpers/translate.js
@@ -0,0 +1,18 @@
+const translatte = require("translatte");
+const { GoogleTranslator } = require("@translate-tools/core/translators/GoogleTranslator");
+
+module.exports = function ({ from = "en", to = "id", text }) {
+  return new Promise((resolve, reject) => {
+    translatte(text, { from, to })
+      .then((res) => resolve(res.text))
+      .catch((err) => {
+        const translator = new GoogleTranslator();
+
+        // Translate single string
+        translator
+          .translate(text, from, to)
+          .then((translate) => resolve(translate))
+          .catch((err) => reject(err));
+      });
+  });
+};
--- routes/web.js
+++ routes/web.js
@@ -5,57 +5,20 @@ const conversationRoute = require("./conversation");
 const passport = require("passport");
 const axios = require("axios");
 const authenticated = passport.authenticate(["jwt", "bearer"], { session: false });
-const translatte = require("translatte");
+const translate = require("../helpers/translate");
+
 const fs = require("fs");
 const csv = require("csv-parser");
-const pso = require("../helpers/PSO");
-const { GoogleTranslator } = require("@translate-tools/core/translators/GoogleTranslator");
 
 router.get("/", async (req, res) => {
   result = "await Session.find({})";
   res.json({ result });
 });
 
-router.get("/pso", async (req, res) => {
-  var toleratedError = 0.01; // samller or equal computation error stops algorithm
-  var epochesLimit = 100; // maximal number of executed iterations
-  var particlesCount = 10;
-
-  console.log("[epoch]\t[error]\t\t\t[position]\n");
-
-  var solution = pso.exampleUsage.computeEpoches$2(toleratedError, epochesLimit, particlesCount);
-
-  console.log("-------------------------------");
-  console.log("Solution:");
-  console.log("  error: " + solution.error);
-  console.log("  position: " + solution.position);
-
-  result = "await Session.find({})";
-  res.json({ result });
-});
-
-router.get("/neo-translate", async (req, res) => {
-  result = "await Session.find({})";
-
-  const translator = new GoogleTranslator();
-
-  // Translate single string
-  const translate = await translator.translate("Hello world", "en", "id");
-
-  res.json({ result, translate });
-});
-
-router.get("/translate", async (req, res) => {
-  result = "await Session.find({})";
-  res.json({ result });
-
-  translatte("apakah anda laki-laki?", { from: "id", to: "en" })
-    .then((res) => {
-      console.log(res.text);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+router.get("/translate", (req, res) => {
+  translate({ text: req.body.text, from: "en" }).then((result) => {
+    res.json({ result });
+  });
 });
 
 router.get("/test-import", (req, res) => {
@@ -71,9 +34,9 @@ router.get("/test-import", (req, res) => {
       // for (let i = 0; i < results.length; i++) {
       for (let i = 0; i < 4; i++) {
         const data = results[i];
-        const translated = await translatte(data.message, { from: "en", to: "id" });
-        console.log(translated);
-        translateds.push(translated.text);
+        const translated = await translate({ text: data.message, from: "en", to: "id" });
+        console.log(data);
+        translateds.push(translated);
       }
 
       res.json({ translateds });
